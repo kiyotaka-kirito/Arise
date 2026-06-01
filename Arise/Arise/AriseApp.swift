6dd7478663dae733378a3224c316cca6ac5dfb8e
kiyotaka-kirito <mghtetpai.1999@gmail.com>
2026-06-01 12:16:12 +0700

    Initial commit

---
+++ Arise/Arise/AriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AriseApp.swift
+//  Arise
+//
+//  Created by Kiyotaka Kirito on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
